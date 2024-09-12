172d7d643a171ff9f2457fef22c25110d4914fc8
j-alencar <joao.alencar@univille.br>
2024-09-12 19:38:05 -0300

    first commit

---
+++ src/main/java/br/univille/fso2024graphademia/Fso2024graphademiaApplication.java
@@ -0,0 +1,13 @@
+package br.univille.fso2024graphademia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fso2024graphademiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fso2024graphademiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/univille/fso2024graphademia/Fso2024graphademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.univille.fso2024graphademia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fso2024graphademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
